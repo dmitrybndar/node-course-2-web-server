edb5b8b67a765aca34417203e6db1a60d67d939a
Dmitry Bondar <dmbd@Dmitrys-MacBook.local>
2018-12-24 22:58:00 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMessage: 'Hello, Bro!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad request'
+  });
+});
+
+app.listen(3000);
